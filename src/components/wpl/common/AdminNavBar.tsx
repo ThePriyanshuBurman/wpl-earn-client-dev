f4eae8e64c2a25c27da8d05bdde5039dddae18ee
SimejiyaAkshat <simejiyaakshat1@gmail.com>
2025-03-31 13:38:13 +0530

    Some updated changes

--- src/components/wpl/common/AdminNavBar.tsx
+++ src/components/wpl/common/AdminNavBar.tsx
@@ -25,12 +25,12 @@ export default function AdminNavbar({ toggleSidebar, isSidebarOpen }: AdminNavba
   return (
     <nav className="flex justify-between items-center w-full px-4 sm:px-[3%] h-[8vh] border-b border-border_dark font-medium text-[16px] z-50 fixed top-0 backdrop-filter backdrop-blur-md text-primary_text_dark">
       <div className="flex items-center gap-4">
-        <button
-          onClick={toggleSidebar}
-          className="p-2 hover:bg-green-600/10 rounded-md"
-        >
-          <Menu size={24} />
-        </button>
+          <button
+            onClick={toggleSidebar}
+            className="p-2 hover:bg-green-600/10 rounded-md md:hidden"
+          >
+            <Menu size={24} />
+          </button>
         <Link href={paths.bounties}>
           <div className="flex items-center gap-2 sm:gap-6">
             <img
