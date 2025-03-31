790d22eb4f130c401f07a23d0f3762c61a43a5d8
SimejiyaAkshat <simejiyaakshat1@gmail.com>
2025-03-31 20:22:24 +0530

    Sidebar changes

--- src/components/wpl/common/NavBarProtected.tsx
+++ src/components/wpl/common/NavBarProtected.tsx
@@ -7,8 +7,14 @@ import { paths } from "@/lib/urls";
 import { useUserStore } from "@/app/store";
 import NotificationDropdown from "../home/NotificationDropdown";
 import Link from "next/link";
+import { Menu } from "lucide-react";
 
-export default function NavBarProtected() {
+interface AdminNavbarProps {
+  toggleSidebar: () => void;
+  isSidebarOpen: boolean; // Add this to track sidebar state
+}
+
+export default function NavBarProtected({ toggleSidebar, isSidebarOpen }: AdminNavbarProps) {
   const pathname = usePathname();
   const [navBarLabel, setNavbarLabel] = useState("");
   const [loading, setLoading] = useState(true);
@@ -38,6 +44,12 @@ export default function NavBarProtected() {
 
   return (
     <nav className="flex justify-between items-center w-full px-4 h-[8vh] border-b border-border_dark font-medium text-[16px] z-50 fixed top-0 backdrop-filter backdrop-blur-md text-primary_text_dark">
+      <button
+            onClick={toggleSidebar}
+            className="p-2 hover:bg-green-600/10 rounded-md md:hidden"
+          >
+            <Menu size={24} />
+      </button>
       {/* Logo */}
       <Link href={paths.bounties} className="flex items-center gap-2">
         <img src="/images/svg/wpl-logo-light.svg" className="h-[28px] w-auto" alt="Logo" />
@@ -58,22 +70,6 @@ export default function NavBarProtected() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
-
-      {/* Mobile Dropdown Menu */}
-      <div className={`fixed top-0 right-0 w-64 bg-secondary_dark shadow-lg rounded-lg transition-transform duration-300 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}>
-        <div className="flex flex-col p-4 gap-4">
-          <button onClick={toggleMenu} className="self-end p-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-
-          <Link href={paths.bounties} className="p-2">Bounties</Link>
-          <Link href={paths.grants} className="p-2">Grants</Link>
-          <Link href={paths.wpl_program} className="p-2">WPL Program</Link>
-          <Link href={paths.leaderboard} className="p-2">Leaderboard</Link>
-        </div>
-      </div>
 
       {/* Profile & Notifications */}
       {loading ? (
